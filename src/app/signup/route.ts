cb14598b06b1e22606080f11541a7953f7465fc7
Jozi-Mwangi <mwasjozi@gmail.com>
2023-11-16 21:31:21 +0300

    debugging

--- src/app/signup/route.ts
+++ src/app/signup/route.ts
@@ -4,13 +4,68 @@ import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { NextApiRequest, NextApiResponse } from "next";
 import { useRouter } from "next/navigation";
 import { NextRequest, NextResponse } from "next/server";
+import { URL } from "url";
+
+// const supabaseURL: URL = "https://supabase.com/dashboard/project/pjarxvvodmtpnggmyfaa"
+// const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+
+
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+
+// export async function POST(request: Request) {
+//   const cookieStore = cookies()
+
+//   const supabase = createServerClient(
+//     process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+//     {
+//       cookies: {
+//         get(name: string) {
+//           return cookieStore.get(name)?.value
+//         },
+//         set(name: string, value: string, options: CookieOptions) {
+//           cookieStore.set({ name, value, ...options })
+//         },
+//         remove(name: string, options: CookieOptions) {
+//           cookieStore.set({ name, value: '', ...options })
+//         },
+//       },
+//     }
+//   )
+//     return
+//  }
+// let req : NextRequest 
+// const res = NextResponse.next()
+// const supabase = createMiddlewareClient<Database>({req, res});
+
 
 // let formData:FormDataProps = 
 
 export async function handler(req:NextApiRequest, res:NextApiResponse) {
     const router = useRouter()
-    const supabase = createMiddlewareClient<Database>({req, res});
+  
+    const cookieStore = cookies()
 
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          cookieStore.set({ name, value, ...options })
+        },
+        remove(name: string, options: CookieOptions) {
+          cookieStore.set({ name, value: '', ...options })
+        },
+      },
+    }
+  )
+  
     if(req.method == "POST"){
         try {
             const {email, password, phoneNumber, userName, userType, selectedGender } = req.body
@@ -18,7 +73,8 @@ export async function handler(req:NextApiRequest, res:NextApiResponse) {
                 email,
                 password,
                 options: {
-                    emailRedirectTo: `${location.origin}/auth/callback`,
+                    // emailRedirectTo: `${location.origin}/auth/callback`,
+                    emailRedirectTo: `${req.headers.origin}/auth/callback`,
                     data: {
                         updated_at: Date.now(),
                         phone_number: phoneNumber,
