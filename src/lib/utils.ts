7de2876cfb00fa78caa98bac8c0f89f15dfbb2e1
Jozi-Mwangi <mwasjozi@gmail.com>
2023-11-27 10:35:04 +0300

    login with google creation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,19 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+import { Database } from "@/types/supabaseTypes"
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function getURL() {
+  let url = 
+  process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+  process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+  'http://localhost:3000/';
+
+  url.includes("http")? url : `https//${url}`;
+  // Including the trailing /
+  url.charAt(url.length -1) === "/" ? url : `${url}/`;
+  return url;
 }
